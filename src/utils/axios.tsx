dd049315bedcf28b53813919ecd829630de01270
reewaz401 <39514982+reewaz401@users.noreply.github.com>
2024-02-17 23:18:35 +0100

    re done base

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Your custom base URL
+  baseURL: 'http://167.86.121.201:5000', // Your custom base URL
 });
 
 export default axiosInstance;
